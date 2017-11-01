51d965b6b43d253d962048ac9bf2da2025f1eea1
Cat Janowitz and Sam Kissee <sam@>
2017-11-01 08:39:18 -0700

    make initial html file and add first parts of backend logic in js

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// 1 - 6
+var rollDice = Math.floor(Math.random() * 6) + 1;
+
+function PlayerOne(score){
+  this.score = score
+};
+function PlayerTwo(score){
+  this.score = score
+};
+
+$(document).ready(function() {
+
+});
